dbf50a023369d26816bc4065f331c393caee499b
Owaish Ansari <hackerowaish@gmail.com>
2026-01-11 18:05:13 +0530

    final update on this project

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# **What `"classes": ["collapse"]` does (short):**
+
+# It **collapses (hides)** that fieldset in the **Django admin UI**.
+
+# **Result:**
+
+# * “Date information” section is **collapsed by default**
+# * Admin user can **click to expand** it
+
+# **Used for:**
+# Keeping the admin form **clean and less cluttered**.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -6,8 +6,19 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     
+    #updated this function, bcoz it returns true even for future publications which is wrong.
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+#corrected function.
+
+# now = timezone.now() → current time
+
+# now - timedelta(days=1) → time 24 hours ago
+
+# Returns True if pub_date is between those two times
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
     def __str__(self):
         return self.question_text
